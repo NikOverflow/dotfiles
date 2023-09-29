bb3071290be96c14886ae027aa3b0ddf9849976f
LetsPlayNik <niklasst2102@gmail.com>
2023-09-29 20:08:23 +0200

    fixed the install script for the .config folder.
    Signed-off-by: LetsPlayNik <niklasst2102@gmail.com>

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ scriptPath="$(dirname $0)"
 cd "$scriptPath/"
 
 mkdir -pm 700 $HOME/Pictures
-mkdir -pm 700 $HOME/.config/{hypr}
 
-#install -m 600 Pictures/* $HOME/Pictures/
 cp -f Pictures/Wallpaper.png $HOME/Pictures/Wallpaper.png
 chown $USER:$USER $HOME/Pictures/Wallpaper.png
 chmod 600 $HOME/Pictures/Wallpaper.png
@@ -13,3 +11,7 @@ chmod 600 $HOME/Pictures/Wallpaper.png
 cp -f startHyprland.sh $HOME/startHyprland.sh
 chown $USER:$USER $HOME/startHyprland.sh
 chmod 700 $HOME/startHyprland.sh
+
+cp -rf .config $HOME/.config
+chown -R $USER:$USER $HOME/.config
+chmod -R 600 $HOME/.config
