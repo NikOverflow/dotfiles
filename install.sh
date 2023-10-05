92962a57cb063117946f5950dbbb35346720a846
LetsPlayNik <niklasst2102@gmail.com>
2023-10-05 17:30:49 +0200

    added neofetch configuration and set font_family in kitty.conf
    Signed-off-by: LetsPlayNik <niklasst2102@gmail.com>

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 scriptPath="$(dirname $0)"
 cd "$scriptPath/"
 
-mkdir -pm 700 $HOME/Pictures $HOME/.scripts $HOME/.config/hypr $HOME/.config/wofi $HOME/.config/kitty
+mkdir -pm 700 $HOME/Pictures $HOME/.scripts $HOME/.config/hypr $HOME/.config/wofi $HOME/.config/kitty $HOME/.config/neofetch
 
 cp -f Pictures/Wallpaper.png $HOME/Pictures/Wallpaper.png
 chown $USER:$USER $HOME/Pictures/Wallpaper.png
@@ -31,3 +31,7 @@ chmod 600 $HOME/.config/wofi/*
 cp -f .config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 chown $USER:$USER $HOME/.config/kitty/kitty.conf
 chmod 600 $HOME/.config/kitty/kitty.conf
+
+cp -f .config/neofetch/config.conf $HOME/.config/neofetch/config.conf
+chown $USER:$USER $HOME/.config/neofetch/config.conf
+chmod 600 $HOME/.config/neofetch/config.conf
